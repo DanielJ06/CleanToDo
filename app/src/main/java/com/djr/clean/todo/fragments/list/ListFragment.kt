1db9ea97c558189edea47b390ade0f9027437978
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-06-26 10:38:41 -0300

    Navigation components fixes

--- app/src/main/java/com/djr/clean/todo/fragments/list/ListFragment.kt
+++ app/src/main/java/com/djr/clean/todo/fragments/list/ListFragment.kt
@@ -5,14 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.djr.clean.todo.R
+import com.djr.clean.todo.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_list, container, false)
+    ): View {
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+
+        binding.fbButton.setOnClickListener {
+            val action = ListFragmentDirections.actionListFragmentToAddFragment()
+            findNavController().navigate(action)
+        }
+
+        return binding.root
     }
 
 }
